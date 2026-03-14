0fdde245d72186539ea82f29d35f63117d2eae60
xiongyiduo <2459807898@qq.com>
2026-03-14 11:03:07 +0800

    assert_20

--- eval_asm.py
+++ eval_asm.py
@@ -28,6 +28,10 @@ FALLBACK_SIGNATURES = [
     "extern double func0(double);",
     "extern double func0(double, double);",
     "extern int func0(int, float*, int);",
+
+    "extern char* func0(char**, int);",  # 最标准匹配 HumanEval/12
+    "extern char* func0(char**, long);", # 部分 64 位汇编可能将长度视为 long
+    "extern const char* func0(char**, int);",
 ]
 
 def build_test_code(func_decl, assert_lines):
